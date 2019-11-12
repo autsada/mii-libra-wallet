56c636aa5cde8093b1505ec0a74acf16de732fe3
autsada <autsada.t@gmail.com>
2019-11-12 09:22:41 +0700

    Fixed useQueryState

--- src/hooks/useQueryState.js
+++ src/hooks/useQueryState.js
@@ -1,13 +1,13 @@
-import { useEffect, useContext, useState } from 'react'
-import { useQuery } from '@apollo/react-hooks'
+import { useEffect, useContext, useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
 
-import { QueryContext } from '../hooks'
-import { QUERY_BY_ADDRESS } from '../apolloClient/query'
-import { saveLocalAccount } from '../helpers/getLocalStorageData'
+import { QueryContext } from "../hooks";
+import { QUERY_BY_ADDRESS } from "../apolloClient/query";
+import { saveLocalAccount } from "../helpers/getLocalStorageData";
 
 export const useQueryState = accountState => {
-  const { setState } = useContext(QueryContext)
-  const [checkState, setCheckState] = useState(false)
+  const { setState } = useContext(QueryContext);
+  const [checkState, setCheckState] = useState(false);
 
   //   const client = useApolloClient()
 
@@ -16,46 +16,49 @@ export const useQueryState = accountState => {
       address: accountState && accountState.address
     },
     pollInterval: 10000
-  })
+  });
 
-  const queryState = () => {
-    return new Promise((resolve, reject) => {
-      let state
-      let count = 0
+  // const queryState = () => {
+  //   return new Promise((resolve, reject) => {
+  //     let state;
+  //     let count = 0;
 
-      const query = setInterval(() => {
-        count++
-        if (data) {
-          if (
-            data.queryByAddress &&
-            data.queryByAddress.response_items &&
-            data.queryByAddress.response_items[0] &&
-            data.queryByAddress.response_items[0].get_account_state_response &&
-            data.queryByAddress.response_items[0].get_account_state_response
-              .account_state_with_proof
-          ) {
-            const {
-              blob: {
-                blob: { balance, sequence_number }
-              }
-            } = data.queryByAddress.response_items[0].get_account_state_response.account_state_with_proof
+  //     const query = setInterval(() => {
+  //       count++;
+  //       if (data) {
+  //         if (
+  //           data.queryByAddress &&
+  //           data.queryByAddress.response_items &&
+  //           data.queryByAddress.response_items[0] &&
+  //           data.queryByAddress.response_items[0].get_account_state_response &&
+  //           data.queryByAddress.response_items[0].get_account_state_response
+  //             .account_state_with_proof
+  //         ) {
+  //           const {
+  //             blob: {
+  //               blob: { balance, sequence_number }
+  //             }
+  //           } = data.queryByAddress.response_items[0].get_account_state_response.account_state_with_proof;
 
-            state = { balance, sequence_number }
+  //           state = { balance, sequence_number };
 
-            clearInterval(query)
-            resolve(state)
-          }
-        }
+  //           clearInterval(query);
+  //           resolve(state);
+  //         }
+  //       }
 
-        if (count > 20 && error) {
-          state = null
+  //       if (count > 10) {
+  //         console.log("Error -->", count);
+  //         state = null;
 
-          clearInterval(query)
-          reject(error)
-        }
-      }, 200)
-    })
-  }
+  //         clearInterval(query);
+  //         reject(state);
+  //       }
+  //     }, 100);
+
+  //     console.log(count);
+  //   });
+  // };
 
   useEffect(() => {
     // Waiting by counting to wait for query to make sure we got latest status of the account
@@ -73,23 +76,23 @@ export const useQueryState = accountState => {
           blob: {
             blob: { balance, sequence_number }
           }
-        } = data.queryByAddress.response_items[0].get_account_state_response.account_state_with_proof
+        } = data.queryByAddress.response_items[0].get_account_state_response.account_state_with_proof;
 
         // Find account state in the system
         const updatedUser = {
           ...accountState,
           balance,
           sequenceNumber: sequence_number
-        }
+        };
 
         // Update context
-        setState(updatedUser)
+        setState(updatedUser);
 
         // Confirm that the state is check
-        setCheckState(true)
+        setCheckState(true);
 
         // Update localStorage
-        saveLocalAccount(updatedUser)
+        saveLocalAccount(updatedUser);
 
         // Update cache
         //   client.writeData({
@@ -101,46 +104,60 @@ export const useQueryState = accountState => {
     }
 
     if (error) {
-      queryState()
-        .then(res => {
-          if (res) {
-            const { balance, sequence_number } = res
+      console.log("Catch error", error.message);
+      // No account found in the system
+      const updatedUser = {
+        ...accountState,
+        balance: "0",
+        sequenceNumber: undefined
+      };
 
-            // Find account state in the system
-            const updatedUser = {
-              ...accountState,
-              balance,
-              sequenceNumber: sequence_number
-            }
+      setState(updatedUser);
 
-            // Update context
-            setState(updatedUser)
+      // Confirm that the state is check
+      setCheckState(true);
 
-            // Confirm that the state is check
-            setCheckState(true)
+      // queryState()
+      //   .then(res => {
+      //     if (res) {
+      //       const { balance, sequence_number } = res;
 
-            // Update localStorage
-            saveLocalAccount(updatedUser)
-          }
-        })
-        .catch(err => {
-          // No account found in the system
-          const updatedUser = {
-            ...accountState,
-            balance: '0',
-            sequenceNumber: undefined
-          }
+      //       // Find account state in the system
+      //       const updatedUser = {
+      //         ...accountState,
+      //         balance,
+      //         sequenceNumber: sequence_number
+      //       };
 
-          setState(updatedUser)
+      //       // Update context
+      //       setState(updatedUser);
 
-          // Confirm that the state is check
-          setCheckState(true)
-        })
+      //       // Confirm that the state is check
+      //       setCheckState(true);
+
+      //       // Update localStorage
+      //       saveLocalAccount(updatedUser);
+      //     }
+      //   })
+      //   .catch(err => {
+      //     console.log("Catch error");
+      //     // No account found in the system
+      //     const updatedUser = {
+      //       ...accountState,
+      //       balance: "0",
+      //       sequenceNumber: undefined
+      //     };
+
+      //     setState(updatedUser);
+
+      //     // Confirm that the state is check
+      //     setCheckState(true);
+      //   });
     }
-  }, [data, error])
+  }, [data, error]);
 
   return {
     checkState,
     setCheckState
-  }
-}
+  };
+};
